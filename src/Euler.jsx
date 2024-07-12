8bbd9025c76b5b531497a02e6c258e41b9768c89
XenexJoshi <xenexjoshi7@gmail.com>
2024-07-12 16:06:58 -0500

    Updated function to allow all form of polynomial functions
    - Added 6 input for each polynomial in form of At^a + Bt^b + Ct^c
    - Updated t, y to dictionary that hold dict representation of  a polynomial function wrt t,y.

--- src/Euler.jsx
+++ src/Euler.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import Plot from 'react-plotly.js'
 
 function Euler() {
-  const [t, set_t] = useState(0);
-  const [y, set_y] = useState(0);
+  const [t, set_t] = useState({ A: 0, B: 0, C: 0, a: 0, b: 0, c: 0 });
+  const [y, set_y] = useState({ A: 0, B: 0, C: 0, a: 0, b: 0, c: 0 });
   const [init_t, set_t0] = useState(0);
   const [init_y, set_y0] = useState(0);
   const [step, set_step] = useState(10);
@@ -12,28 +12,62 @@ function Euler() {
   const [y_axis, setY] = useState([]);
   const [y_res, setYR] = useState(0);
 
-  function handle_t(event) {
-    set_t(parseInt(event.target.value));
+  function handle_tA(event) {
+    set_t(e => ({ ...e, A: parseFloat(event.target.value) }));
   }
 
-  function handle_y(event) {
-    set_y(parseInt(event.target.value));
+  function handle_ta(event) {
+    set_t(e => ({ ...e, a: parseFloat(event.target.value) }));
+  }
+  function handle_tB(event) {
+    set_t(e => ({ ...e, B: parseFloat(event.target.value) }));
+  }
+
+  function handle_tb(event) {
+    set_t(e => ({ ...e, b: parseFloat(event.target.value) }));
+  }
+  function handle_tC(event) {
+    set_t(e => ({ ...e, C: parseFloat(event.target.value) }));
+  }
+  function handle_tc(event) {
+    set_t(e => ({ ...e, c: parseFloat(event.target.value) }));
+  }
+
+  function handle_yA(event) {
+    set_y(e => ({ ...e, A: parseFloat(event.target.value) }));
+  }
+
+  function handle_ya(event) {
+    set_y(e => ({ ...e, a: parseFloat(event.target.value) }));
+  }
+  function handle_yB(event) {
+    set_y(e => ({ ...e, B: parseFloat(event.target.value) }));
+  }
+
+  function handle_yb(event) {
+    set_y(e => ({ ...e, b: parseFloat(event.target.value) }));
+  }
+  function handle_yC(event) {
+    set_y(e => ({ ...e, C: parseFloat(event.target.value) }));
+  }
+  function handle_yc(event) {
+    set_y(e => ({ ...e, c: parseFloat(event.target.value) }));
   }
 
   function handle_t0(event) {
-    set_t0(parseInt(event.target.value));
+    set_t0(parseFloat(event.target.value));
   }
 
   function handle_y0(event) {
-    set_y0(parseInt(event.target.value));
+    set_y0(parseFloat(event.target.value));
   }
 
   function handle_step(event) {
-    set_step(parseInt(event.target.value));
+    set_step(parseFloat(event.target.value));
   }
 
   function handle_dest(event) {
-    set_dest(parseInt(event.target.value));
+    set_dest(parseFloat(event.target.value));
   }
 
   function solve() {
@@ -43,7 +77,9 @@ function Euler() {
     let t_table = [init_t];
     let y_table = [init_y];
     for (let i = 0; i < step; ++i) {
-      temp_y += h * (y * temp_y + t * temp_t);
+      temp_y += (h * (t.A * Math.pow(temp_t, t.a) + t.B * Math.pow(temp_t, t.b) +
+        t.C * Math.pow(temp_t, t.C) + y.A * Math.pow(temp_y, y.a) +
+        y.B * Math.pow(temp_y, t.a) + y.C * Math.pow(temp_y, t.c)))
       temp_t += h;
       t_table.push(temp_t);
       y_table.push(temp_y);
@@ -52,15 +88,28 @@ function Euler() {
     setY(y_table);
   }
 
+
   return (
     <div className="wrapper">
       <div className="input">
         <h1> Euler Approximation to First Order ODE.</h1>
         <div>
-          <p> Enter the coefficient of t:</p>
-          <input type="number" value={t} onChange={handle_t} />
-          <p> Enter the coefficient of y:</p>
-          <input type="number" value={y} onChange={handle_y} />
+          <p> Assign values for A, x, B, y, C, z as they appear in
+            A.t<sup> x</sup> + B.t<sup> y</sup> + C.t<sup> z</sup>:</p>
+          A: <input type="number" value={t.A} onChange={handle_tA} />
+          x: <input type="number" value={t.a} onChange={handle_ta} />
+          B: <input type="number" value={t.B} onChange={handle_tB} />
+          y: <input type="number" value={t.b} onChange={handle_tb} />
+          C: <input type="number" value={t.C} onChange={handle_tC} />
+          z: <input type="number" value={t.c} onChange={handle_tc} />
+          <p> Assign values for A, p, B, q, C, r as they appear in
+            A.y<sup> p</sup> + B.y<sup> q</sup> + C.y<sup> r</sup>:</p>
+          A: <input type="number" value={y.A} onChange={handle_yA} />
+          p: <input type="number" value={y.a} onChange={handle_ya} />
+          B: <input type="number" value={y.B} onChange={handle_yB} />
+          q: <input type="number" value={y.b} onChange={handle_yb} />
+          C: <input type="number" value={y.C} onChange={handle_yC} />
+          r: <input type="number" value={y.c} onChange={handle_yc} />
           <p> Enter the starting coordinate (t, y):</p>
           <input type="number" value={init_t} onChange={handle_t0} />
           <input type="number" value={init_y} onChange={handle_y0} />
@@ -97,5 +146,5 @@ function Euler() {
   )
 }
 
-// A1 tx + A2 tx + B1 yx + B2 yx + C + Dsin kt + D sin ky + Ee ^ kt
+// Dsin kt + D sin ky + Ee ^ kt
 export default Euler
